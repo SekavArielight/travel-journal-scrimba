82dde0225f4fbc7971fc55234241dfe4bb287bef
God'swill Sekav Apya <godswillapya@gmail.com>
2025-06-16 19:07:18 +0100

    Initialize travel journal project with React, Vite, and ESLint setup; add header component and styles; include globe and marker images.

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <img src="/globe.png" alt="Globe" width="40px"/>
+      <h1>my travel journal.</h1>
+    </header>
+  );
+};
+
+export default Header;
