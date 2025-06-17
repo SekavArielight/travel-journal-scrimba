f37bab673cbe439ff1567035658714e216363cf7
God'swill Sekav Apya <godswillapya@gmail.com>
2025-06-17 15:23:02 +0100

    Add Entry component to display travel journal details for Mount Fuji

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import Header from './components/Header'
+import React from "react";
+import Header from "./components/Header";
+import Entry from "./components/Entry";
 
 const App = () => {
   return (
-    <Header/>
-  )
-}
+    <>
+      <Header />
+      <Entry />
+    </>
+  );
+};
 
-export default App
+export default App;
