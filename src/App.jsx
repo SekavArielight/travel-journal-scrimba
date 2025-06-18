bb239df255acadaf5ef51e092034fbd3a8e1c6f3
God'swill Sekav Apya <godswillapya@gmail.com>
2025-06-18 15:43:37 +0100

    Implement Entry component with dynamic data rendering and add travel journal data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import Header from "./components/Header";
 import Entry from "./components/Entry";
+import data from "./constants/data";
 
 const App = () => {
+  const entryElements = data.map((entry) => {
+    return <Entry
+     img={entry.img}
+     title={entry.title}
+     country={entry.country}
+     googleMapsLink={entry.googleMapsLink}
+     dates={entry.dates}
+     text={entry.text}
+    />
+  });
   return (
     <>
       <Header />
       <main className="container">
-        <Entry />
+        {entryElements}
       </main>
     </>
   );
